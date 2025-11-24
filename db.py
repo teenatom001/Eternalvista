56a1c0c7f3ce3d9c8cb147b5168c2b64f90deb68
TeenaTom2001 <20024072@mydbs.ie>
2025-11-24 23:21:32 +0000

    db connection

--- db.py
+++ db.py
@@ -1,10 +1,21 @@
 import mysql.connector
+from mysql.connector import Error
 
 def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
-        user="root",         
-        password="root",  
+        user="root",
+        password="root",
         database="eternalvista"
     )
     return connection
+
+
+
+if __name__ == "__main__":
+    try:
+        conn = get_db_connection()
+        print("Database connected successfully!")
+        conn.close()
+    except Error as e:
+        print("Connection failed:", e)
